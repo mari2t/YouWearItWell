2eefbc47c33db418bb814fe396407c31fe211e8d
mari2t <subcodeoft@gmail.com>
2023-05-15 20:06:18 +0900

    気温を追加

--- src/app/Forecast.js
+++ src/app/Forecast.js
@@ -5,8 +5,15 @@ import axios from "axios";
 const Home = () => {
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState(null);
+  const [celsius, setCelsius] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+
+  const kelvinToCelsius = (data) => {
+    const constantKelvin = 273.15;
+    const currentCelsius = parseInt(data - constantKelvin, 10);
+    setCelsius(currentCelsius);
+  };
 
   const fetchWeather = async (e) => {
     e.preventDefault();
@@ -16,12 +23,14 @@ const Home = () => {
       const response = await axios.get(
         `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.NEXT_PUBLIC_API_KEY}`
       );
+      kelvinToCelsius(response.data.main.feels_like);
       setWeather(response.data);
       console.log(response.data);
     } catch (error) {
       setError("Failed to fetch weather data");
+    } finally {
+      setLoading(false);
     }
-    setLoading(false);
   };
 
   return (
@@ -44,6 +53,7 @@ const Home = () => {
           <div>
             <h3>{weather.name}</h3>
             <p>{weather.weather[0].description}</p>
+            <p>{celsius}°C</p>
             <img
               src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
               alt="Weather icon"
@@ -54,5 +64,4 @@ const Home = () => {
     </div>
   );
 };
-
 export default Home;
