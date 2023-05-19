d27ccbd636ce55312806ba9da5a4f8908ca4ac59
mari2t <subcodeoft@gmail.com>
2023-05-19 20:05:47 +0900

    温度ごとの洋服を提示

--- src/app/Forecast.js
+++ src/app/Forecast.js
@@ -2,6 +2,21 @@
 import React, { useState } from "react";
 import axios from "axios";
 
+// API base URLs
+const WEATHER_API_BASE_URL =
+  "http://api.openweathermap.org/data/2.5/weather?q=";
+const FORECAST_API_BASE_URL =
+  "http://api.openweathermap.org/data/2.5/forecast?q=";
+
+const TEMPERATURE_CLOTHES_MAP = [
+  { min: 26, image: "/img/nosleeve.jpg" },
+  { min: 22, image: "/img/shortsleeve.jpg" },
+  { min: 17, image: "/img/longsleeve.jpg" },
+  { min: 12, image: "/img/sweater.jpg" },
+  { min: 6, image: "/img/coat.jpg" },
+  { min: null, image: "/img/glovesAndKnitHat.jpg" },
+];
+
 const Home = () => {
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState(null);
@@ -19,7 +34,7 @@ const Home = () => {
       const encodedCity = encodeURIComponent(city);
 
       const response = await axios.get(
-        `http://api.openweathermap.org/data/2.5/weather?q=${encodedCity}&appid=${process.env.NEXT_PUBLIC_API_KEY}`
+        `${WEATHER_API_BASE_URL}${encodedCity}&appid=${process.env.NEXT_PUBLIC_API_KEY}`
       );
       setWeather({
         ...response.data,
@@ -30,7 +45,7 @@ const Home = () => {
       });
 
       const responseFiveDays = await axios.get(
-        `http://api.openweathermap.org/data/2.5/forecast?q=${encodedCity}&appid=${process.env.NEXT_PUBLIC_API_KEY}`
+        `${FORECAST_API_BASE_URL}${encodedCity}&appid=${process.env.NEXT_PUBLIC_API_KEY}`
       );
       const newForecast = responseFiveDays.data.list.slice(0, 16).map((el) => ({
         time: el.dt_txt,
@@ -42,6 +57,16 @@ const Home = () => {
       setError(`Failed to fetch weather data: ${error.toString()}`);
     } finally {
       setLoading(false);
+    }
+  };
+
+  // Choose appropriate clothes based on temperature
+  const clothesImage = (temperature) => {
+    for (let i = 0; i < TEMPERATURE_CLOTHES_MAP.length; i++) {
+      const { min, image } = TEMPERATURE_CLOTHES_MAP[i];
+      if (!min || temperature >= min) {
+        return image;
+      }
     }
   };
 
@@ -69,6 +94,10 @@ const Home = () => {
             <img
               src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
               alt="Weather icon"
+            />
+            <img
+              src={clothesImage(weather.main.feels_like)}
+              alt="Appropriate clothes"
             />
             <h4>Today:</h4>
             <h5>{new Date().toLocaleString()}</h5>
