1977af981353c4bd8e97d16e223186681eedd52e
mari2t <subcodeoft@gmail.com>
2023-07-06 21:53:09 +0900

    環境変数を戻してみる

--- src/app/api/forecast/route.js
+++ src/app/api/forecast/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export async function GET(request) {
   const encodedCity = request.nextUrl.searchParams.get("encodedCity");
   const res = await fetch(
-    `${process.env.FORECAST_API_BASE_URL}${encodedCity}&cnt=10&appid=${process.env.NEXT_API_KEY}&lang=ja&units=metric`
+    `${process.env.FORECAST_API_BASE_URL}${encodedCity}&cnt=10&appid=${process.env.NEXT_PUBLIC_API_KEY}&lang=ja&units=metric`
   );
 
   const data = await res.json();
