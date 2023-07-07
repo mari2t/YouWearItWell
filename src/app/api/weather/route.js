0a9369024177308384ab45b608ca9f0c690ae336
mari2t <subcodeoft@gmail.com>
2023-07-07 19:33:02 +0900

    環境変数名を変更

--- src/app/api/weather/route.js
+++ src/app/api/weather/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export async function GET(request) {
   const encodedCity = request.nextUrl.searchParams.get("encodedCity");
   const res = await fetch(
-    `${process.env.WEATHER_API_BASE_URL}${encodedCity}&appid=${process.env.NEXT_PUBLIC_API_KEY}&lang=ja&units=metric`
+    `${process.env.WEATHER_API_BASE_URL}${encodedCity}&appid=${process.env.WEATHER_API_KEY}&lang=ja&units=metric`
   );
 
   const data = await res.json();
